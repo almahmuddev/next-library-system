d12306737cea18f5d9623e5b2e88c1a0ae98e259
almahmuddev <devalmahmud@gmail.com>
2026-06-02 12:18:04 +0600

    use local images for all book covers

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const nextConfig = {
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
       { protocol: "https", hostname: "avatars.githubusercontent.com" },
       { protocol: "https", hostname: "images.unsplash.com" },
+      // { protocol: "https", hostname: "covers.openlibrary.org" },   // added images locally.
     ],
   },
 };
